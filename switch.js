9d5baefb7dc5537ffc024c4dd8b8d5761635ba3f
ceciliawambui <47850604+ceciliawambui@users.noreply.github.com>
2020-01-25 17:55:10 +0300

    used switch in this exercise

---
+++ switch.js
@@ -0,0 +1,14 @@
+switch(prompt("What is the weather like?")){
+    case "rainy":
+       console.log("Remember to bring an umbrella.");
+       break;
+    case "rainy":
+       console.log("Dress lightly.");
+       break;
+    case "rainy":
+       console.log("Go outside.");
+       break;
+    default:
+       console.log("Unknown weather type!");
+       break;
+}
