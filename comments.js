f9765d94846fa85f6845d4c31f20630126b1978e
ceciliawambui <47850604+ceciliawambui@users.noreply.github.com>
2020-01-25 17:55:46 +0300

    wrote comments on this program

---
+++ comments.js
@@ -0,0 +1,8 @@
+let accountBalance = calculateBalance(account);
+//It's a greeen hollow where a river sings
+accountBalance.adjust();
+//Madly catchiing white tatters in the grass.
+let report = new report();
+//Where the sun on the proud mountain rings:
+addToReport(accountBalance, report);
+//It's a little valley, foaming like the light in a glass.
